9f07f4042f67a16a6c0e8ef2d8aca3d2a225a0ee
Marcos Silva <marcosantoniio177171@gmail.com>
2026-08-15 23:50:09 +0000

    feat: adiciona método de excluir e sua rota (#1)

--- cadastro_temporadas/app.py
+++ cadastro_temporadas/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///temporadas.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
 
 class Temporada(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,7 +20,9 @@ class Temporada(db.Model):
     episodios = db.relationship(
         "Episodio",
         backref="temporada",
-        lazy=True)
+        lazy=True
+    )
+
 
 class Episodio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,7 +34,9 @@ class Episodio(db.Model):
     temporada_id = db.Column(
         db.Integer,
         db.ForeignKey("temporada.id"),
-        nullable=False)
+        nullable=False
+    )
+
 
 @app.route("/temporadas/cadastrar", methods=["GET", "POST"])
 def cadastrar_temporada():
@@ -53,6 +58,7 @@ def cadastrar_temporada():
         return "Temporada cadastrada com sucesso!"
 
     return render_template("cadastrar_temporada.html")
+
 
 @app.route("/episodio/cadastrar", methods=["GET", "POST"])
 def cadastrar_episodio():
@@ -77,7 +83,10 @@ def cadastrar_episodio():
     temporadas = Temporada.query.all()
 
     return render_template(
-        "cadastrar_episodio.html", temporadas=temporadas)
+        "cadastrar_episodio.html",
+        temporadas=temporadas
+    )
+
 
 @app.route("/temporada/editar/<int:id>", methods=["GET", "POST"])
 def editar_temporada(id):
@@ -85,21 +94,23 @@ def editar_temporada(id):
     temporada = Temporada.query.get_or_404(id)
 
     if request.method == "POST":
-        temporada.titulo=request.form["titulo"]
-        temporada.descricao=request.form["descricao"]
-        temporada.data_publicacao=datetime.strptime(
+        temporada.titulo = request.form["titulo"]
+        temporada.descricao = request.form["descricao"]
+        temporada.data_publicacao = datetime.strptime(
             request.form["data_publicacao"],
             "%Y-%m-%d"
-        ).date(),
-        temporada.status=request.form["status"]
+        ).date()
+        temporada.status = request.form["status"]
 
         db.session.commit()
 
         return "Temporada editada com sucesso!"
+
     return render_template(
         "editar_temporada.html",
-        temporadas=temporadas
+        temporada=temporada
     )
+
 
 @app.route("/episodio/editar/<int:id>", methods=["GET", "POST"])
 def editar_episodio(id):
@@ -107,23 +118,27 @@ def editar_episodio(id):
     episodio = Episodio.query.get_or_404(id)
 
     if request.method == "POST":
-        episodio.numero=request.form["numero"]
-        episodio.titulo=request.form["titulo"]
-        episodio.descricao=request.form["descricao"]
-        episodio.data_publicacao=datetime.strptime(
+        episodio.numero = request.form["numero"]
+        episodio.titulo = request.form["titulo"]
+        episodio.descricao = request.form["descricao"]
+        episodio.data_publicacao = datetime.strptime(
             request.form["data_publicacao"],
             "%Y-%m-%d"
-        ).date(),
-        episodio.temporada_id=request.form["temporada_id"]
-        
+        ).date()
+        episodio.temporada_id = request.form["temporada_id"]
+
         db.session.commit()
 
-        return "Temporada editada com sucesso!"
+        return "Episodio editado com sucesso!"
+
+    temporadas = Temporada.query.all()
+
     return render_template(
         "editar_episodio.html",
         episodio=episodio,
         temporadas=temporadas
     )
+
 
 @app.route("/temporadas")
 def listar_temporadas():
@@ -134,17 +149,40 @@ def listar_temporadas():
         temporadas=temporadas
     )
 
+
 @app.route("/episodios")
 def listar_episodios():
-    episodio = Episodio.query.all()
+    episodios = Episodio.query.all()
 
     return render_template(
-        "episodio.html",
+        "episodios.html",
         episodios=episodios
     )
 
+
+@app.route("/temporadas/excluir/<int:id>", methods=["POST"])
+def excluir_temporada(id):
+    temporada = Temporada.query.get_or_404(id)
+
+    db.session.delete(temporada)
+    db.session.commit()
+
+    return redirect(url_for("listar_temporadas"))
+
+
+@app.route("/episodio/excluir/<int:id>", methods=["POST"])
+def excluir_episodio(id):
+    episodio = Episodio.query.get_or_404(id)
+
+    db.session.delete(episodio)
+    db.session.commit()
+
+    return redirect(url_for("listar_episodios"))
+
+
 with app.app_context():
     db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
